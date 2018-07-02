11fd5d89b121ec3c33c80cdd13ed239d06832de2
thanabodeekrathu <thanabodee.thkt@gmail.com>
2018-07-02 19:10:45 +0700

    delete unuse action

--- app/javascript/ng-app/second_id/second_id.component.ts
+++ app/javascript/ng-app/second_id/second_id.component.ts
@@ -43,31 +43,4 @@ export class Second_IdComponent {
       console.log(e);
     })
   }
-
-  update(id, string_test, integer_test, boolean_test) {
-    this.attrs = {
-      string_test: string_test,
-      integer_test: integer_test,
-      boolean_test: boolean_test
-    }
-    this.myDataService.update(id, this.attrs).subscribe(resp => {
-      console.log(resp);
-      this.myDatas = resp;
-    }, e => {
-      console.log(e);
-    })
-  }
-
-  create(newMyData) {
-    console.log(newMyData.getCreateParam())
-    this.myDataService.create(newMyData.getCreateParam()).subscribe(resp => {
-      console.log(resp);
-      this.myDatas = resp;
-      this.newMyData = new MyData();
-    }, e => {
-      console.log(e);
-    })
-  }
-
-
 }
